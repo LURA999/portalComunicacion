c458eef94e1938423464f336a9c5e4b3eb92c89e
LURA999 <57641653+LURA999@users.noreply.github.com>
2023-03-13 01:48:35 -0700

    ultimo cambio del portal, mejorando la tecnica de eliminacion de las imagenes del slide y noticias

--- src/app/inicio/popup/eliminar/eliminar.component.ts
+++ src/app/inicio/popup/eliminar/eliminar.component.ts
@@ -2,7 +2,7 @@ import { Component, Inject, OnInit,  } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { SubirImgVideoService } from 'src/app/core/services/img-video.service';
 import { imgVideoModel } from 'src/app/interfaces_modelos/img-video.model';
-import { concatMap, lastValueFrom, Subscription } from 'rxjs';
+import { concatMap, lastValueFrom, of, Subscription } from 'rxjs';
 import { ResponseInterfaceTs } from 'src/app/interfaces_modelos/response.interface';
 import { UsuarioService } from 'src/app/core/services/usuario.service';
 import { AuthService } from 'src/app/core/services/auth.service';
@@ -40,7 +40,9 @@ export class EliminarComponent implements OnInit {
 
       switch (this.data.seccion.toString()) {
         case "foto/imagen":
-          if (this.data.opc === true) {
+          this.contenedor_carga.style.display = "block";
+
+          /*if (this.data.opc === true) {
             await lastValueFrom(this.serviceImgVideo.eliminarImgVideo(Number(this.data.id),"imgVideoNoticia"))
             this.$sub.add(this.serviceImgVideo.todoImgVideo("imgVideoNoticia",-1,1,0,-1).subscribe(async (resp : ResponseInterfaceTs)=>{
               this.dialogRef.close(await resp.container===undefined?[]:resp.container);
@@ -55,10 +57,62 @@ export class EliminarComponent implements OnInit {
               idS:0
             }
             await lastValueFrom(this.servImgVideo.eliminarPosicionTDSlide(deleteSlide));
+
+
             this.$sub.add(this.serviceImgVideo.todoImgVideo("imgVideo",-1,1,0,-1).subscribe(async (resp : ResponseInterfaceTs)=>{
               this.dialogRef.close(await resp.container===undefined?[]:resp.container);
             }))
-          }
+          }*/
+
+          of('').pipe(
+            concatMap(()=>{
+              if (this.data.opc === true) {
+              return this.serviceImgVideo.eliminarImgVideo(Number(this.data.id),"imgVideoNoticia").pipe(
+                concatMap(() =>
+                this.serviceImgVideo.todoImgVideo("imgVideoNoticia",-1,1,0,-1)
+              ))
+              }else{
+                return this.serviceImgVideo.eliminarImgVideo(Number(this.data.obj.idImgVideo),"imgVideo").pipe(concatMap(()=>{
+                let deleteSlide : dosParamInt = {
+                  cveLocal: Number(this.data.obj.cveLocal),
+                  cveSeccion: Number(this.data.obj.cveSeccion),
+                  idP: Number(this.data.obj.posicion),
+                  idS:0
+                }
+                return this.servImgVideo.eliminarPosicionTDSlide(deleteSlide).pipe(concatMap(()=>
+                this.serviceImgVideo.todoImgVideo("imgVideo",-1,1,0,-1)
+                ))
+              }))
+              }
+
+            })).subscribe((resp : ResponseInterfaceTs)=>{
+              this.dialogRef.close( resp.container===undefined?[]:resp.container)
+              this.contenedor_carga.style.display = "none";
+
+            })
+            
+            /*  await lastValueFrom(this.serviceImgVideo.eliminarImgVideo(Number(this.data.id),"imgVideoNoticia"))
+              this.$sub.add(this.serviceImgVideo.todoImgVideo("imgVideoNoticia",-1,1,0,-1).subscribe(async (resp : ResponseInterfaceTs)=>{
+                this.dialogRef.close(await resp.container===undefined?[]:resp.container);
+              }))
+            } else {
+  
+              await lastValueFrom(this.serviceImgVideo.eliminarImgVideo(Number(this.data.obj.idImgVideo),"imgVideo"))
+              let deleteSlide : dosParamInt = {
+                cveLocal: Number(this.data.obj.cveLocal),
+                cveSeccion: Number(this.data.obj.cveSeccion),
+                idP: Number(this.data.obj.posicion),
+                idS:0
+              }
+              await lastValueFrom(this.servImgVideo.eliminarPosicionTDSlide(deleteSlide));
+  
+  
+              this.$sub.add(this.serviceImgVideo.todoImgVideo("imgVideo",-1,1,0,-1).subscribe(async (resp : ResponseInterfaceTs)=>{
+                this.dialogRef.close(await resp.container===undefined?[]:resp.container);
+              }))
+            }
+          */
+
           break;
         case "usuario":
             this.servImgVideo.eliminarDirImgUsuario(this.data.id).pipe(
