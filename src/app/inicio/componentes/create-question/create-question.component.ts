2a6a8ed795389662f5be8d2299920b57936dac64
Alonso Luna <alonsolr1999@gmail.com>
2024-09-11 11:07:29 -0700

    Cambio de rutas

--- src/app/inicio/componentes/create-question/create-question.component.ts
+++ src/app/inicio/componentes/create-question/create-question.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-create-question',
   templateUrl: './create-question.component.html',
-  styleUrl: './create-question.component.css'
+  styleUrl: './create-question.component.css',
+  standalone: true,
+  imports: [MatGridListModule],
 })
 export class CreateQuestionComponent {
 
